952fe7052b0c2be44b43a289b8bdb97b9cb9e427
diana <dianabagrin9777@gmail.com>
2021-08-10 11:40:04 +0300

    Afost adaugat un btn_nou, madificat backgroundul si timpul de afishare a textului

---
+++ app/src/main/java/com/example/exemplu_button/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.exemplu_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1, btn2, btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.btn1);
+
+        Button btn2 = (Button)findViewById(R.id.btn2);
+        Button btn3 = (Button)findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Butonul 1",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Butonul 2",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Butonul 3",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
